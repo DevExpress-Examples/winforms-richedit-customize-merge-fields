bda2f9bf0f331968b4ff35b58bd2dc963a16a909
DevExpressExampleBot <fakeEmail>
2023-12-05 18:33:07 +0400

    Source auto update [skip ci]

--- VB/Form1.vb
+++ VB/Form1.vb
@@ -1,96 +1,100 @@
-Imports Microsoft.VisualBasic
 Imports System
 Imports System.Collections.Generic
 Imports System.Windows.Forms
 Imports DevExpress.XtraRichEdit.API.Native
 
 Namespace RichEditCustomizeMergeFields
-	Partial Public Class Form1
-		Inherits Form
-		Public Sub New()
-			InitializeComponent()
 
-			Dim invoices As New List(Of Invoice)(10)
+    Public Partial Class Form1
+        Inherits Form
 
-			invoices.Add(New Invoice(0, "Invoice1", 10.0D, Guid.NewGuid()))
-			invoices.Add(New Invoice(1, "Invoice2", 15.0D, Guid.NewGuid()))
-			invoices.Add(New Invoice(2, "Invoice3", 20.0D, Guid.NewGuid()))
+        Public Sub New()
+            InitializeComponent()
+            Dim invoices As List(Of Invoice) = New List(Of Invoice)(10)
+            invoices.Add(New Invoice(0, "Invoice1", 10.0D, Guid.NewGuid()))
+            invoices.Add(New Invoice(1, "Invoice2", 15.0D, Guid.NewGuid()))
+            invoices.Add(New Invoice(2, "Invoice3", 20.0D, Guid.NewGuid()))
+            richEditControl1.Options.MailMerge.DataSource = invoices
+        End Sub
 
-			richEditControl1.Options.MailMerge.DataSource = invoices
-		End Sub
+        Private Sub richEditControl1_CustomizeMergeFields(ByVal sender As Object, ByVal e As DevExpress.XtraRichEdit.CustomizeMergeFieldsEventArgs)
+            Dim mergeFieldNames As List(Of MergeFieldName) = New List(Of MergeFieldName)(e.MergeFieldsNames)
+            mergeFieldNames.Remove(mergeFieldNames.Find(Function(mfn) Equals(mfn.Name.ToLower(), "password")))
+            mergeFieldNames.ForEach(New Action(Of MergeFieldName)(AddressOf ChangeDisplayName))
+            mergeFieldNames.Sort(New ReverseComparer())
+            e.MergeFieldsNames = mergeFieldNames.ToArray()
+        End Sub
 
-		Private Sub richEditControl1_CustomizeMergeFields(ByVal sender As Object, ByVal e As DevExpress.XtraRichEdit.CustomizeMergeFieldsEventArgs) Handles richEditControl1.CustomizeMergeFields
-			Dim mergeFieldNames As New List(Of MergeFieldName)(e.MergeFieldsNames)
+        Private Shared Sub ChangeDisplayName(ByVal mfn As MergeFieldName)
+            mfn.DisplayName += " (field)"
+        End Sub
+    End Class
 
-			mergeFieldNames.Remove(mergeFieldNames.Find(Function(mfn) mfn.Name.ToLower() = "password"))
-			mergeFieldNames.ForEach(AddressOf ChangeDisplayName)
-			mergeFieldNames.Sort(New ReverseComparer())
+    Public Class ReverseComparer
+        Implements IComparer(Of MergeFieldName)
 
-			e.MergeFieldsNames = mergeFieldNames.ToArray()
-		End Sub
+'#Region "IComparer<MergeFieldName> Members"
+        Public Function Compare(ByVal mfn1 As MergeFieldName, ByVal mfn2 As MergeFieldName) As Integer Implements IComparer(Of MergeFieldName).Compare
+            Return -String.Compare(mfn1.Name, mfn2.Name)
+        End Function
+'#End Region
+    End Class
 
-		Private Shared Sub ChangeDisplayName(ByVal mfn As MergeFieldName)
-			mfn.DisplayName &= " (field)"
-		End Sub
-	End Class
+    Public Class Invoice
 
-	Public Class ReverseComparer
-		Implements IComparer(Of MergeFieldName)
-		#Region "IComparer<MergeFieldName> Members"
-		Public Function Compare(ByVal mfn1 As MergeFieldName, ByVal mfn2 As MergeFieldName) As Integer Implements IComparer(Of MergeFieldName).Compare
-			Return -String.Compare(mfn1.Name, mfn2.Name)
-		End Function
-		#End Region
-	End Class
+        Private idField As Integer
 
-	Public Class Invoice
-		Private id_Renamed As Integer
+        Public Property Id As Integer
+            Get
+                Return idField
+            End Get
 
-		Public Property Id() As Integer
-			Get
-				Return id_Renamed
-			End Get
-			Set(ByVal value As Integer)
-				id_Renamed = value
-			End Set
-		End Property
-		Private description_Renamed As String
+            Set(ByVal value As Integer)
+                idField = value
+            End Set
+        End Property
 
-		Public Property Description() As String
-			Get
-				Return description_Renamed
-			End Get
-			Set(ByVal value As String)
-				description_Renamed = value
-			End Set
-		End Property
-		Private price_Renamed As Decimal
+        Private descriptionField As String
 
-		Public Property Price() As Decimal
-			Get
-				Return price_Renamed
-			End Get
-			Set(ByVal value As Decimal)
-				price_Renamed = value
-			End Set
-		End Property
-		Private password_Renamed As Guid
+        Public Property Description As String
+            Get
+                Return descriptionField
+            End Get
 
-		Public Property Password() As Guid
-			Get
-				Return password_Renamed
-			End Get
-			Set(ByVal value As Guid)
-				password_Renamed = value
-			End Set
-		End Property
+            Set(ByVal value As String)
+                descriptionField = value
+            End Set
+        End Property
 
-		Public Sub New(ByVal id As Integer, ByVal description As String, ByVal price As Decimal, ByVal password As Guid)
-			Me.Id = id
-			Me.Description = description
-			Me.Price = price
-			Me.Password = password
-		End Sub
-	End Class
+        Private priceField As Decimal
 
+        Public Property Price As Decimal
+            Get
+                Return priceField
+            End Get
+
+            Set(ByVal value As Decimal)
+                priceField = value
+            End Set
+        End Property
+
+        Private passwordField As Guid
+
+        Public Property Password As Guid
+            Get
+                Return passwordField
+            End Get
+
+            Set(ByVal value As Guid)
+                passwordField = value
+            End Set
+        End Property
+
+        Public Sub New(ByVal id As Integer, ByVal description As String, ByVal price As Decimal, ByVal password As Guid)
+            Me.Id = id
+            Me.Description = description
+            Me.Price = price
+            Me.Password = password
+        End Sub
+    End Class
 End Namespace
